1157edc2fe270d912ab6207b7b74b6a46bb15373
Alexxpinheiroo <alexmateus27@gmail.com>
2024-09-06 22:38:53 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "A Fadinha do skate, Rayssa Leal, é uma skatista profissional brasileira, vice-campeã olímpica em Tóquio 2020. Conquistou o mundo com seu talento e carisma, tornando-se um ícone do esporte.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira, campeã olímpica em Tóquio 2020. Conquistou a medalha de ouro no individual geral, tornando-se a primeira brasileira a conquistar o ouro olímpico na ginástica artística.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira, com diversas medalhas em competições nacionais e internacionais. Sua habilidade e determinação a tornaram uma das principais atletas do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judô judoca"
+    }
+];
